bacdf215724f99bf47cf541cebf423b064a8646c
VanDuc111 <ducca94tk@gmail.com>
2026-01-29 10:26:17 +0700

    Initial commit for blog website

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Duc's Blog";
+export const SITE_DESCRIPTION = `Bờ lốc bờ lock. "Thời nay ai còn viết blog nữa, search AI một cái là xong".
+ Tôi cũng nghĩ vậy nhưng kệ đi, tôi sẽ viết mấy thứ linh tinh trong đầu tôi vào đây.`;
